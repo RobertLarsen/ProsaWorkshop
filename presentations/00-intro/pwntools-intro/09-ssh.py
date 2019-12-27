208aad58b9d7b0a8ba229073d7722d78f078ad23
Robert Larsen <robert@the-playground.dk>
2019-12-27 21:32:57 +0100

    Removed spaces

--- presentations/00-intro/pwntools-intro/09-ssh.py
+++ presentations/00-intro/pwntools-intro/09-ssh.py
@@ -2,9 +2,9 @@
 
 from pwn import *
 
-context(log_level = 'error')
+context(log_level='error')
 
-doing = term.output(float = True)
+doing = term.output(float=True)
 
 solutions = (
     'cat $HOME/readme',
@@ -14,16 +14,18 @@ solutions = (
     'for file in inhere/*; do file -- ${file}; done | grep ASCII | awk -F: \'{print $1}\' | xargs cat'
 )
 
+
 def solve(name, password, cmd):
     doing.update('Solving %s' % name)
-    con = ssh(user = name, password = password, host = 'bandit.labs.overthewire.org', port = 2220)
-    shell = con.shell(tty = False)
+    con = ssh(user=name, password=password, host='bandit.labs.overthewire.org', port=2220)
+    shell = con.shell(tty=False)
     shell.sendline(cmd)
     next_password = shell.recvline().strip()
     shell.close()
     con.close()
     doing.update('%s gave us %s\n' % (name, next_password))
     return next_password
+
 
 password = 'bandit0'
 for i in range(0, len(solutions)):
